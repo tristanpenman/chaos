dc5fc12e78a4c857f35f74009a5b68000cd080cb
Tristan Penman <tristan@tristanpenman.com>
2021-07-03 20:15:24 +1000

    msvc: variable name stragglers

--- msvc/src/Main.cpp
+++ msvc/src/Main.cpp
@@ -7,7 +7,7 @@ Application g_application;
 HMENU g_menu_chaos = 0;
 HMENU g_menu_level = 0;
 
-int WINAPI WinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR szCmdLine, _In_ int iCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hinst_prev, _In_ PSTR cmd_line, _In_ int cmd_show)
 {
     // menu shown when the app first loads
     g_menu_chaos = LoadMenu(hinst, MAKEINTRESOURCE(ID_MENU_CHAOS));
@@ -15,7 +15,7 @@ int WINAPI WinMain(_In_ HINSTANCE hinst, _In_opt_ HINSTANCE hPrevInstance, _In_ 
     // menu shown when a level is loaded and an MDI instance is open
     g_menu_level = LoadMenu(hinst, MAKEINTRESOURCE(ID_MENU_LEVEL));
 
-    int result = g_application.run(hinst, hPrevInstance, szCmdLine, iCmdShow);
+    int result = g_application.run(hinst, hinst_prev, cmd_line, cmd_show);
 
     DestroyMenu(g_menu_chaos);
     g_menu_chaos = 0;
